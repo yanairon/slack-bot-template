0ed523ae13bfa1066b7c936aa36ec4f803ba8ed9
yanai_r <yanair@traxretail.com>
2020-04-11 13:55:20 +0300

    Various fixes

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
-const fs = require("fs");
+const fs = require('fs');
 
 // Dynamically load routes from /routes
-const routePath = "./lib/routes/";
-fs.readdirSync(routePath).forEach(function(file) {
+const routePath = './lib/routes/';
+fs.readdirSync(routePath).forEach((file) => {
     const route = routePath + file;
+    // eslint-disable-next-line global-require
     require(route)(app);
 });
 
-app.listen(5000, function () {
+app.listen(5000, () => {
+    // eslint-disable-next-line no-console
     console.log('Node server is running on port 5000');
 });
